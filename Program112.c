76e6aa08324a6d326fd59e09ef2446942dc9b83b
PatilLina <linapatil0304@gmail.com>
2025-12-12 10:26:55 +0530

    Add files via upload

---
+++ Program112.c
@@ -0,0 +1,21 @@
+//call by value program
+#include<stdio.h>
+
+void Display(int *ptr)//issue pointer ++ 
+{
+    int iCnt = 0;
+
+ for(iCnt = 0; iCnt < 4; iCnt++)
+ {
+     printf("%d\n",*ptr);
+ } 
+}
+
+int main()
+{
+  int Arr[] = {10,20,30,40};
+
+  Display(Arr);    //Display(100)
+  
+  return 0;
+}
